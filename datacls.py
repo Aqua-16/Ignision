b8aa859ebb706133932b634a14bfbe7ed8c84af4
Aqua-16 <aquafrost16@gmail.com>
2024-02-17 19:45:46 +0530

    Define necessary data structs

---
+++ datacls.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+import numpy as np
+from typing import List
+from typing import Tuple
+
+@dataclass
+class Box:
+    class_index : int
+    class_name : str
+    corners : np.ndarray
+
+    def __repr__(self):
+        return "[class=%s (%f,%f,%f,%f)]" % (self.class_name, self.corners[0], self.corners[1], self.corners[2], self.corners[3])
+
+    def __str__(self):
+        return repr(self)
+
+@dataclass
+class TrainingSample:
+    anchor_map: np.ndarray 
+    gt_rpn_map: np.ndarray
+    gt_rpn_object_indices: List[Tuple[int,int,int]]
+    gt_rpn_background_indices: List[Tuple[int,int,int]]
+    gt_boxes: List[Box]
+    image_data: np.ndarray
+    image : np.ndarray
+    filepath: str
