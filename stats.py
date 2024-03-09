d1a340714a038e15bd31a27e1ab4fd39371703b1
sahaj-96 <sahajsrivastava96@gmail.com>
2024-03-09 15:46:01 +0530

    Fixed name error

--- stats.py
+++ stats.py
@@ -18,8 +18,9 @@ class train_statistics:
         self.detector_regression_losses=[]
 
     def during_training_step(self,losses):#used once in every iteration to aggregate losses
+        print(losses.keys())
         self.rpn_class_losses.append(losses["rpn_class_loss"])
-        self.rpn_regression_losses.append(losses["rpn_regression_loss"])
+        self.rpn_regression_losses.append(losses["rpn_reg_loss"])
         self.detector_class_losses.append(losses["detector_class_loss"])
         self.detector_regression_losses.append(losses["detector_regression_loss"])
         self.rpn_class_loss=np.mean(self.rpn_class_losses)
